6c4a2de19263e93326c75e23c1b79d7c371ec510
in8e <nateboardman99@gmail.com>
2023-02-21 12:48:33 +0000

    Edited app.js/css for better UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,43 +2,63 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const [data, setData] = React.useState([])
+  const [data, setData] = React.useState([]);
 
   React.useEffect(() => {
     fetch("/questions")
       .then((res) => res.json())
       .then((data) => setData(data))
       .catch((error) => console.log(error))
-}, []);
-      
-      const shuffle = (array) => {
-        for (let i = array.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-        
-      };
-        return (
-          <div className="page">
-            {data && data.length > 0 ? (
-              data.map((item) => {
-                const answers = shuffle([item.correct, item.alphaincorrect, item.bravoincorrect, item.charlieincorrect]);
-                return (
-                  <div key={item.id}>
-                    <h2>{item.question}</h2>
-                    {answers.map((answer, index) => (
-                      <div className="answers"><button key={index}>{answer}</button></div>
-                    ))}
+      .catch(console.log(data.questions));
+  }, []);
+
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+  return (
+    <div className="pageContainer">
+      <h1 className="Header">Full stack quiz</h1>
+      <div className="page">
+        {data && data.length > 0 ? (
+          data.map((item) => {
+            const answers = shuffle([
+              item.correct,
+              item.alphaincorrect,
+              item.bravoincorrect,
+              item.charlieincorrect,
+            ]);
+            return (
+              <div key={item.id}>
+                <h2 className="questionPrompt">{item.question}</h2>
+                {answers.map((answer, index) => (
+                  <div className="answers">
+                    <button key={index}>{answer}</button>
                   </div>
-                );
-              })
-            ) : (
-              <p>No data</p>
-            )}
-            
-          </div>
-        )
-    };
+                ))}
+              </div>
+            );
+          })
+        ) : (
+          <p>No data</p>
+        )}
+      </div>
+      <div className="Footer">
+        <p>
+          Created by{" "}
+          <a className="gitLink" href="https://github.com/in8e">
+            Nate Boardman
+          </a>
+        </p>
+        <p className="footerNote">
+          This was created using React, Node, Express, and PostgreSQL.
+        </p>
+      </div>
+    </div>
+  );
+}
 
 export default App;
